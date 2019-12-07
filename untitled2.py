3389aa33fdd61867db0223a9c3580f8ae3b5a735
Mehadi Hassan <mehadihassan116@gmail.com>
2019-12-07 09:59:28 +0600

    gui

---
+++ untitled2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 09:49:44 2019
+
+@author: Mehadi Hassan
+"""
+
+from tkinter import *
+root = Tk()
+root.title("Sentiment Analysis")
+root.geometry("640x640+0+0")
+
+
+
+
+
+root.mainloop()
